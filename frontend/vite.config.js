2a981712064d449774d01b7567c5c7bd4f6f0293
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2025-02-07 18:35:45 +0100

    fix(frontend): build config for legacy browsers

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: false,
   plugins: [
     react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'], // Adjust for your needs
+    }),
   ],
   build: {
+    target: 'es2015',
     // lib: {
     //   entry: "./src/index.js",
     //   name: "ReactFileManager",
