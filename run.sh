dc02ba8af4ee5301dfe40e631dba8a67e49c3b45
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2025-02-19 18:18:18 +0100

    Revert "feat(run): exit on error"
    This reverts commit 5f072d94e88157862b74acb991f8931cf3e5552a.

--- run.sh
+++ run.sh
@@ -1,18 +1,7 @@
-RUN() {
-    OUTPUT=$("$@" 2>&1)  # Capture both stdout and stderr
-    EXIT_CODE=$?
-    echo "> $@"
-    if [ $EXIT_CODE -ne 0 ]; then
-        echo "$OUTPUT"  # Print the command output
-        echo "!!! ERROR"
-        exit $EXIT_CODE  # Exit if the command failed
-    fi
-}
-
 echo "\n--- Creating mongodb user and resetting collection..."
-RUN docker cp backend/mongodb-init.js playerok-mongodb:/mongodb-init.js
+docker cp backend/mongodb-init.js playerok-mongodb:/mongodb-init.js
 sleep 1 # wait until mongo get up
-RUN docker exec -it playerok-mongodb mongo mongodb-init.js
+docker exec -it playerok-mongodb mongo mongodb-init.js
 
 echo "\n--- Running..."
 cd backend
